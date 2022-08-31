7e464ae4945e5cfcc670343aee52245ab222053d
Carriane <carrianelinzer@icloud.com>
2022-08-31 13:49:54 -0500

    TailwindCss

---
+++ app.js
@@ -0,0 +1,17 @@
+function addition(x, y) {
+    console.log(x+y);
+}
+
+funtion subtraction(x, y) {
+    
+}
+
+function mutiply(x, y) {
+    return x*=y;
+} 
+
+function division(x, y) {
+    return x/=y; 
+}
+
+addition(7, 4);
